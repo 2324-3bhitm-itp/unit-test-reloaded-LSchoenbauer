6019efd19d4da600a6b361bdda165f34071df7cf
linnea_schoenbauer <l.schoenbauer@students.htl-leonding.ac.at>
2023-12-19 21:02:26 +0100

    added Garden, Plant and PlantType

--- src/main/java/at/ac/htl/leonding/bhitm3/Garden.java
+++ src/main/java/at/ac/htl/leonding/bhitm3/Garden.java
@@ -1,5 +1,29 @@
 package at.ac.htl.leonding.bhitm3;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Garden {
 
+    private static int idCounter = 0;
+
+    private List<Plant> plants = new LinkedList<>();
+    private int id;
+
+    public Garden() {
+        id = idCounter++;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+    }
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    public int getId() {
+        return id;
+    }
+    
 }
